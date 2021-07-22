4a4a46e35e14dcda4b56dc4669f07e99d248229e
Mark Toda <toda.mark@gmail.com>
2021-07-22 16:49:08 -0700

    fix: total supply
    The total supply fetch was getting incorrect values. This commit fixes
    that bug

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -48,25 +48,3 @@ export function fetchTokenName(tokenAddress: Address): string {
 
   return nameValue;
 }
-
-export function fetchTokenTotalSupply(tokenAddress: Address): BigInt {
-  let contract = ERC20.bind(tokenAddress);
-  let totalSupplyValue = null;
-  let totalSupplyResult = contract.try_totalSupply();
-  if (!totalSupplyResult.reverted) {
-    totalSupplyValue = totalSupplyResult as i32;
-  }
-  return BigInt.fromI32(totalSupplyValue as i32);
-}
-
-export function fetchTokenDecimals(tokenAddress: Address): BigInt {
-  let contract = ERC20.bind(tokenAddress);
-  // try types uint8 for decimals
-  let decimalValue = null;
-  let decimalResult = contract.try_decimals();
-  if (!decimalResult.reverted) {
-    decimalValue = decimalResult.value;
-  }
-
-  return BigInt.fromI32(decimalValue as i32);
-}

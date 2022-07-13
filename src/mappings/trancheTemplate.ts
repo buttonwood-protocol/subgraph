ff2db178ce997406a0b4d87245c7bbacb22b79f6
Fiddlekins <fiddlekins@gmail.com>
2022-07-13 17:22:46 +0100

    - fixed account balances

--- src/mappings/trancheTemplate.ts
+++ src/mappings/trancheTemplate.ts
@@ -1,5 +1,7 @@
 import { Transfer, } from '../../generated/templates/TrancheTemplate/Tranche';
-import { updateAccountBalance } from '../utils/accountBalance';
+import { fetchAccountBalance } from '../utils/accountBalance';
+import { isNullAddress } from '../utils';
+import { fetchAccount } from '../utils/account';
 
 export function handleTransfer(event: Transfer): void {
   const from = event.params.from;
@@ -7,6 +9,23 @@ export function handleTransfer(event: Transfer): void {
   const tokenId = event.address.toHexString();
   const amount = event.params.value;
 
-  updateAccountBalance(from, tokenId, amount, event);
-  updateAccountBalance(to, tokenId, amount, event);
+  if (!isNullAddress(from)) {
+    let fromAccount = fetchAccount(from.toHexString());
+    let fromAccountBalance = fetchAccountBalance(fromAccount.id, tokenId);
+    fromAccountBalance.amount = fromAccountBalance.amount.minus(amount);
+    fromAccountBalance.block = event.block.number;
+    fromAccountBalance.modified = event.block.timestamp;
+    fromAccountBalance.transaction = event.transaction.hash;
+    fromAccountBalance.save();
+  }
+
+  if (!isNullAddress(to)) {
+    let toAccount = fetchAccount(to.toHexString());
+    let toAccountBalance = fetchAccountBalance(toAccount.id, tokenId);
+    toAccountBalance.amount = toAccountBalance.amount.plus(amount);
+    toAccountBalance.block = event.block.number;
+    toAccountBalance.modified = event.block.timestamp;
+    toAccountBalance.transaction = event.transaction.hash;
+    toAccountBalance.save();
+  }
 }
--- src/utils/accountBalance.ts
+++ src/utils/accountBalance.ts
@@ -1,9 +1,5 @@
-import { Address, BigInt } from '@graphprotocol/graph-ts';
 import { AccountBalance } from '../../generated/schema';
 import { ZERO_BI } from './constants';
-import { isNullAddress } from './index';
-import { fetchAccount } from './account';
-import { Transfer } from '../../generated/templates/TrancheTemplate/Tranche';
 
 export function createAccountBalance(accountId: string, tokenId: string): AccountBalance {
   const accountBalanceId = `${accountId}-${tokenId}`;
@@ -22,16 +18,4 @@ export function fetchAccountBalance(accountId: string, tokenId: string): Account
     accountBalance = createAccountBalance(accountId, tokenId);
   }
   return accountBalance;
-}
-
-export function updateAccountBalance(accountAddress: Address, tokenId: string, amount: BigInt, event: Transfer): void {
-  if (!isNullAddress(accountAddress)) {
-    const account = fetchAccount(accountAddress.toHexString());
-    const accountBalance = fetchAccountBalance(account.id, tokenId);
-    accountBalance.amount = accountBalance.amount.minus(amount);
-    accountBalance.block = event.block.number;
-    accountBalance.modified = event.block.timestamp;
-    accountBalance.transaction = event.transaction.hash;
-    accountBalance.save();
-  }
 }

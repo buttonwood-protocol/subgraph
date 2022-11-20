212456596c726810bfe469e5a19cdcff7424545b
Fiddlekins <fiddlekins@gmail.com>
2022-11-20 00:15:37 +0000

    WIP

---
+++ src/acceptedFactoryAddresses.ts
@@ -0,0 +1,6 @@
+import { Address } from '@graphprotocol/graph-ts';
+
+export const acceptedFactoryAddresses = [
+  Address.fromString('0xda5dbe504e7d532e4f8921b38e1f970d4b881bfb'),
+  Address.fromString('0xdbb8717d53d03e5dbb979540b6a85c65770f0709')
+];
--- src/mappings/bondFactory.ts
+++ src/mappings/bondFactory.ts
@@ -1,10 +1,23 @@
-import { BigInt } from '@graphprotocol/graph-ts';
+import { Address, BigInt } from '@graphprotocol/graph-ts';
 import { BondCreated } from '../../generated/BondFactory/BondFactory';
 import { BondTemplate } from '../../generated/templates';
+import { acceptedFactoryAddresses } from '../acceptedFactoryAddresses';
 import { createBond } from '../utils/bond';
 import { fetchFactory } from '../utils/factory';
 
+function isAcceptedFactoryAddress(address: Address): boolean {
+  for (let i = 0; i < acceptedFactoryAddresses.length; i++) {
+    if (address == acceptedFactoryAddresses[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
 export function handleBondCreated(event: BondCreated): void {
+  if (!isAcceptedFactoryAddress(event.address)) {
+    return;
+  }
   const factory = fetchFactory(event.address);
   factory.bondCount = factory.bondCount.plus(BigInt.fromI32(1));
   factory.save();

62c5c6aa0660aa70b0025eb4dd0640181ed24e69
Fiddlekins <fiddlekins@gmail.com>
2022-11-20 02:00:59 +0000

    - added build script which overrides src with network specific files - this enables filtering factories by specific addresses that differ between networks - removed kovan scripts since that testnet is dead

--- src/acceptedFactoryAddresses.ts
+++ src/acceptedFactoryAddresses.ts
@@ -1,6 +1,7 @@
 import { Address } from '@graphprotocol/graph-ts';
 
 export const acceptedFactoryAddresses = [
-  Address.fromString('0xda5dbe504e7d532e4f8921b38e1f970d4b881bfb'),
-  Address.fromString('0xdbb8717d53d03e5dbb979540b6a85c65770f0709')
+  Address.fromString('0x019FA32d71Bb96922695C6cdeA33774Fdeb04Ac0'),
+  Address.fromString('0xfc74f7b04C620DaD6a750d4F60F13586E2b1EF54'),
+  Address.fromString('0x72799FFD1F4CCF92eA2b1eE0CADa16a5461c4d96')
 ];

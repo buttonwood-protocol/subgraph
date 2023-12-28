dd5fab42b2b953723a0f3c1b4f70f7d2033a3060
SocksNFlops <socks&flops@prl.one>
2023-12-28 09:09:59 -0500

    feat(bond-factory): adding new bond-factory addresses to be tracked

--- src-networks/goerli/acceptedFactoryAddresses.ts
+++ src-networks/goerli/acceptedFactoryAddresses.ts
@@ -2,5 +2,6 @@ import { Address } from '@graphprotocol/graph-ts';
 
 export const acceptedFactoryAddresses = [
   Address.fromString('0x23052d2DE04dA53c742aB8175f1De94b3d335d51'),
-  Address.fromString('0x5318ca0345bC070D05b0B480E8b11F7e8Ac76720')
+  Address.fromString('0x5318ca0345bC070D05b0B480E8b11F7e8Ac76720'),
+  Address.fromString('0x98babbD6B3CfD2542B3401B376880E8b078B8091')
 ];
